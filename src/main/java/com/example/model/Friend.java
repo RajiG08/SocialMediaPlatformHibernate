d43b66a2a69bf0c73c1fb103321481e37a5ce1fa
Rajalakshmi G <66936135+RajiG08@users.noreply.github.com>
2025-02-07 13:05:41 +0530

    Create Friend.java

---
+++ src/main/java/com/example/model/Friend.java
@@ -0,0 +1,55 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "friends")
+public class Friend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "friend_id")
+    private int friendId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "friend_user_id", nullable = false)
+    private User friendUser;
+
+    // Getters and Setters
+    public int getFriendId() {
+        return friendId;
+    }
+
+    public void setFriendId(int friendId) {
+        this.friendId = friendId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getFriendUser() {
+        return friendUser;
+    }
+
+    public void setFriendUser(User friendUser) {
+        this.friendUser = friendUser;
+    }
+
+    public Friend(User user, User friendUser) {
+        this.user = user;
+        this.friendUser = friendUser;
+    }
+
+    public Friend() {
+        super();
+    }
+}
