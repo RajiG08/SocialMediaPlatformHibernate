f1e7f202ae2735062ecb9794433d70c8eeab98f6
Rajalakshmi G <66936135+RajiG08@users.noreply.github.com>
2025-02-07 13:05:01 +0530

    Create Comment.java

---
+++ src/main/java/com/example/model/Comment.java
@@ -0,0 +1,68 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private int commentId;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "comment_text")
+    private String commentText;
+
+    // Getters and Setters
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+	public Comment(Post post, User user, String commentText) {
+		super();
+		this.post = post;
+		this.user = user;
+		this.commentText = commentText;
+	}
+
+	public Comment() {
+		super();
+	}
+}
